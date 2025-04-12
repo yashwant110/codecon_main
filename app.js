9b7e8330e3e41f8299aea158e4906ba25d27e53b
yashwant110 <byashwanth000@gmail.com>
2025-04-12 23:09:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const challengeRoutes = require('./routes/challengeRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+const bepoRoutes = require('./routes/bepoRoutes');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes); // e.g., /api/auth/signup
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/challenge', challengeRoutes);
+app.use('/api/course', courseRoutes);
+app.use('/api/payment', paymentRoutes);
+app.use('/api/bepo', bepoRoutes);
 
 module.exports = app;
