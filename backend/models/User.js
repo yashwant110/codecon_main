7352fa43af9a94863ab71286b497d7b67fdba166
yashwant110 <byashwanth000@gmail.com>
2025-04-12 14:53:52 +0530

    Delete backend directory

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: String,
-  otp: String,
-  otpExpiry: Date,
-  isPremium: { type: Boolean, default: false }
-}, { timestamps: true });
-module.exports = mongoose.model('User', userSchema);
--- backend/routes/bepoRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bepoController = require('../controllers/bepoController');
-router.post('/recommend', bepoController.recommend);
-module.exports = router;
