7bcba11fa8aa1a28bfdd944e7188ebaefb7d42f1
yashwant110 <byashwanth000@gmail.com>
2025-04-12 23:25:40 +0530

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require('../models/User');
+
+// Controller for updating user profile, fetching user details etc.
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error" });
+  }
+};
+
+module.exports = { getUserProfile };
