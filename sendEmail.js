d9e59763a0ff3c7d56b11f1be0dfa6ad042e2b7d
yashwant110 <byashwanth000@gmail.com>
2025-04-13 02:35:10 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,20 +1,22 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const sendEmail = async (to, otp) => {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
 
-exports.sendEmail = async (to, subject, html) => {
-  const mailOptions = {
+  let info = await transporter.sendMail({
     from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-  };
+    to: to,
+    subject: 'OTP for Password Reset',
+    text: `Your OTP is: ${otp}`
+  });
 
-  await transporter.sendMail(mailOptions);
+  return info;
 };
+
+module.exports = sendEmail;
