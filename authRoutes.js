cfef661526a37903f9db909f8a08fc294a583d31
yashwant110 <byashwanth000@gmail.com>
2025-04-13 00:13:12 +0530

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, forgotPassword, resetPassword } = require('../controllers/authController');
+const { signup, login, forgotPassword, resetPassword } = require('./authController');
 
 // Signup Route
 router.post('/signup', signup);
