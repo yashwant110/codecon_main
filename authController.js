e449f58c70dda000c94f832632180c3604fe652b
yashwant110 <byashwanth000@gmail.com>
2025-04-13 00:16:20 +0530

    Update authController.js

--- authController.js
+++ authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('./User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const sendEmail = require('./sendEmail');
