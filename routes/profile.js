7b1a8a9e205b73139fdd0c274551544be8a68a8a
yashwant110 <byashwanth000@gmail.com>
2025-04-12 11:54:24 +0530

    Delete routes/profile.js

--- routes/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example route
-router.get('/', (req, res) => {
-  res.json({ message: 'User profile route is working!' });
-});
-
-module.exports = router;
