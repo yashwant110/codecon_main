997afa0e08c1a8ac41c5c4f6c3387eac9610355c
yashwant110 <byashwanth000@gmail.com>
2025-04-13 02:40:55 +0530

    Update challengeController.js

--- challengeController.js
+++ challengeController.js
@@ -1,14 +1,20 @@
-const Challenge = require('./Challenge');
-
-const createChallenge = async (req, res) => {
-  try {
-    const { title, description } = req.body;
-    const newChallenge = new Challenge({ title, description });
-    await newChallenge.save();
-    res.status(201).json(newChallenge);
-  } catch (error) {
-    res.status(500).json({ msg: "Server Error" });
+// challengeController.js
+module.exports = {
+  createChallenge: async (req, res) => {
+    try {
+      // Logic to create a new challenge
+      res.status(201).json({ msg: 'Challenge created successfully' });
+    } catch (err) {
+      res.status(500).json({ msg: 'Server Error' });
+    }
+  },
+  
+  getAllChallenges: async (req, res) => {
+    try {
+      // Logic to get all challenges
+      res.status(200).json({ msg: 'All challenges retrieved successfully' });
+    } catch (err) {
+      res.status(500).json({ msg: 'Server Error' });
+    }
   }
 };
-
-module.exports = { createChallenge };
