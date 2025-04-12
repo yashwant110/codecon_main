b7c4754755559bb1e6d3afb1f5b67100e8e1c83b
yashwant110 <byashwanth000@gmail.com>
2025-04-13 00:22:30 +0530

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUserProfile } = require('../controllers/userController');
+const { getUserProfile } = require('./userController');
 const router = express.Router();
 
 router.get('/profile', getUserProfile);
