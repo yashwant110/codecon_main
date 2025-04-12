8fbf0a4a64ec0385d4d74c9f682fec5d48f8a0e7
yashwant110 <byashwanth000@gmail.com>
2025-04-12 14:55:30 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const authRoutes = require('./routes/authRoutes');
-const bepoRoutes = require('./routes/bepoRoutes');
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/bepo', bepoRoutes);
-module.exports = app;
