f4cc9aa69a5adfaaef9e7786c7beba57a131b8fa
yashwant110 <byashwanth000@gmail.com>
2025-04-12 11:52:58 +0530

    Delete routes/auth.js

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example route
-router.get('/', (req, res) => {
-  res.send('Auth route is working!');
-});
-
-module.exports = router;
